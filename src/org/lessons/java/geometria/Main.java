3d77dfa7d941eb04ce776298fea53694f5fbcb06
N4LT4M <m4tt3o.casale@gmail.com>
2023-07-18 17:13:40 +0200

    creazione costruttore

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,20 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Creare una classe Main contenente il metodo main(),
+        nel quale chiedete all’utente di inserire, da console,
+        i valori di base e di altezza con cui istanziare un nuovo Rettangolo.
+         Dopo averlo istanziato, stampate a video il perimetro e l’area.
+         */
+
+        Scanner scan = new Scanner(System.in);
+        int baseUtente = scan.nextInt();
+        int altezzaUtente =scan.nextInt();
+
+
+        Rettangolo r = new Rettangolo(baseUtente, altezzaUtente);
+    }
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,2 +1,22 @@
-package org.lessons.java.geometria;public class rettangolo {
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+    /*una classe Rettangolo con due attributi interi:
+    base e altezza.
+    Aggiungere un opportuno costruttore con parametri.
+    Aggiungere due metodi:
+    calcolaArea e calcolaPerimetro che calcolano e restituiscono,
+    rispettivamente, l’area e il perimetro del rettangolo.
+     */
+    int base;
+    int altezza;
+
+    Rettangolo(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    int calcolaArea(){
+
+    }
 }
